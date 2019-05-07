a7e84b9fe9ca91d8f4e55dc9076c045d16103ee4
Dorothea Mueller <dorothea.mueller01@stud.uni-goettingen.de>
2019-05-07 14:31:19 +0200

    add req for model

--- src/model/kd_tree_and_dict.py
+++ src/model/kd_tree_and_dict.py
@@ -1,6 +1,9 @@
 from .world import World
-
+from scipy.spatial import cKDTree
 
 class KdTreeAndDict(World):
     def __init__(self):
         self.all_objects = {}
+        self.kd_tree = cKDTree()
+
+
