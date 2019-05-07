e8c49d2181b34ccc89e7e68df8469bfa22751eaf
cebudding <celineevianne@gmail.com>
2019-05-07 16:48:02 +0200

    Added functionality for return and backspace key (together with @hyma2194)

--- src/view/input_box.py
+++ src/view/input_box.py
@@ -10,22 +10,23 @@ class InputBox(UIElement):
         self.color2 = color2
         self.color = color1
         self.text = text
-        self.txt_surface = self.view.FONT.render(text, True, self.color)
+        self.txt_surface = self.view.FONT.render(text,True,self.color)
         self.active = False
         self.on("mouseclick", self.mouse_click, self.color2)
-        self.on("keyret", self.keyret, self.text)
-        self.on("keyback", self.keyback, self.text)
+        self.on("keyret", self.keyret,self.text)
+        self.on("keyback", self.keyback,self.text)
 
     def mouse_click(self, newcolor):
         self.active = not self.active
         self.color = newcolor
 
-    def keyret(self, text):
-        print(text)
+    def keyret(self,newtext):
+        print(newtext)
         self.text = ''
 
-    def keyback(self, text):
-        self.text = text[:-1]
+    def keyback(self,newtext):
+        print(newtext)
+        self.text = newtext[:-1]
 
     def event_handler(self, event):
         pygame.init()
@@ -46,26 +47,28 @@ class InputBox(UIElement):
                 self.active = False
         if event.type == pygame.KEYDOWN:
             if self.active:
-                if event.key == pygame.K_BACKSPACE:
+
+                if "keyback" in self.events and event.key == pygame.K_BACKSPACE:
                     self.text = self.text[:-1]
-                    print('back loop')
-                    # if "keyret" in self.events:
-                    #     for fnct, args in self.events["keyret"]:
-                    #         fnct(args)
-                if event.key == pygame.K_RETURN:
-                    print('ret loop')
-                    # if "keyback" in self.events:
-                    #     for fnct, args in self.events["keyback"]:
-                    #         fnct(args)
+                    # for fnct,args in self.events["keyback"]:
+                    #     fnct(args)
+
+                elif "keyret" in self.events and event.key == pygame.K_RETURN:
+                    self.text = ''
+                    print(self.text+'1')
+                    # for fnct,args in self.events["keyret"]:
+                    #     fnct(args)
                 else:
-                    # print('Print text')
                     self.text += event.unicode
-                self.txt_surface = self.view.FONT.render(self.text, True, self.color)
+
+                self.txt_surface = self.view.FONT.render(self.text,True,self.color)
+                print(self.text+'2')
 
     def update(self):
+        pass
         # Resize the box if the text is too long.
-        width = max(200, self.txt_surface.get_width() + 10)
-        self.rect.w = width
+        # width = max(200, self.txt_surface.get_width() + 10)
+        # self.rect.w = width
 
     def draw(self):
         self.view.screen.blit(self.txt_surface, (self.rect.x + 5, self.rect.y + 5))
