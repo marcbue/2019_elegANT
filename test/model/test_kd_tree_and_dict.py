3fcb1b6c3ca8e4ea794ce26771dac1d944665866
Dorothea Mueller <dorothea.mueller01@stud.uni-goettingen.de>
2019-05-09 02:19:03 +0200

    start testing tree by adding fixture and first test

--- test/model/test_kd_tree_and_dict.py
+++ test/model/test_kd_tree_and_dict.py
@@ -1,5 +1,22 @@
 from src.model.kd_tree_and_dict import KdTreeAndDict
+from src.model.nest import Nest
 
-def test_create_nest():
+import pytest
+
+@pytest.fixture
+def set_up_tree_with_nests():
     tree = KdTreeAndDict()
-    assert tree
+    # TODO: can also be random parameters
+    positions = [(0, 0), (-1, -1)]
+    tree.create_nests(['red', 'green'], positions, size=1, health=100)
+    return tree, positions
+
+def test_get_position_2(set_up_tree_with_nests):
+    tree, positions = set_up_tree_with_nests
+    for pos in positions:
+        objects_at_pos = tree.get_at_position(pos)
+
+        # TODO: Yes, there can be several test made out of this.
+        # There should be only 1 nest at a position.
+        assert len(objects_at_pos) == 1
+        assert isinstance(objects_at_pos[0], Nest)
