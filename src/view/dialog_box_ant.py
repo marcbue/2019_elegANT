c64450a428fc9db1193e544ff2a6be6f1f62ec3c
Gabriel <darkmash@web.de>
2019-05-14 17:08:01 +0200

    bugfix so it runs again

--- src/view/dialog_box_ant.py
+++ src/view/dialog_box_ant.py
@@ -1,14 +1,15 @@
 from dialog_box import DialogBox
 import pygame
 
+
 class DialogBoxAnt(DialogBox):
-    def __init__(self,view,identifier,ant):
-        super(self, DialogBoxAnt).__init__(self, view, identifier, x=view.size[0], y=view.size[1]*0.75, width=view.size[0], height=view.size[1]*0.25)
-        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+    def __init__(self, view, identifier, ant):
+        super(DialogBoxAnt, self).__init__(view, identifier, x=view.size[0], y=view.size[1]*0.75, width=view.size[0], height=view.size[1]*0.25)
         self.ant = ant
 
     def draw(self):
         if self.ant.active:
-            for i,stat in enumerate(self.ant.stats):
-                self.view.screen.blit(f"{stat.key} : {stat.value}", (self.rect.x + 5, self.rect.y + i*5))
-            pygame.draw.rect(self.view.screen, self.background_color, self.rect, 2)
+            print(self.rect)
+            for i, stat in enumerate(self.ant.stats):
+                self.view.screen.blit(f"{stat.key} : {stat.value}", (self.rect.x + 5, self.rect.y + (i+1)*5))
+            pygame.draw.rect(self.view.screen, pygame.Color("black"), self.rect, 2)
