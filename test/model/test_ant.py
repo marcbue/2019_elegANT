ad81be670101522be0be5c61a7feca551249e16f
ssn2500 <saeed.salehi.n@gmail.com>
2019-05-21 16:12:27 +0200

    ant move updated and ant test script created

--- test/model/test_ant.py
+++ test/model/test_ant.py
@@ -7,27 +7,26 @@ nest = Nest(position=array([0, 0]), color='red', size=10, health=100)
 ant = Ant(color='red', home_nest=nest)
 
 
-def test_update_has_food():
+def test_move_has_food():
     ant.has_food = True
     ant.position = array([10, 0])
     position = ant.move()
     # asserting that y-move is towards the nest
-    assert np.isclose(position, array([9, 0])) == array([True, True]), 'incorrect y-move direction'
+    assert np.isclose(position, array([9, 0])).all(), 'incorrect y-move direction'
     # asserting that the position is updated
-    assert np.isclose(ant.position, position) == array([True, True]), 'position not updated'
+    assert np.isclose(ant.position, position).all(), 'position not updated'
     ant.position = array([0, 10])
     position = ant.move()
     # asserting that x-move is towards the nest
-    assert np.isclose(position, array([0, 9])) == array([True, True]), 'incorrect x-move direction'
+    assert np.isclose(position, array([0, 9])).all(), 'incorrect x-move direction'
 
 
-def test_update__randomly():
+def test_move_randomly():
     ant.has_food = False
     ant.position = array([0, 0])
     ant.momentum = array([0, 0])
-    for i in range(10):  # trying 10 random walks
-        previous_position = array(ant.position)
-        position = ant.move()
-        assert np.isclose(position, ant.position) == array([True, True]), 'position not updated'
-        assert not np.isclose(1, np.linalg.norm(ant.momentum)), 'momentum not one: %r' % ant.momentum
-        assert not np.isclose(1, np.linalg.norm(ant.position-previous_position)), 'movement not one: %r' % ant.position
+    previous_position = array(ant.position)
+    position = ant.move()
+    assert np.isclose(position, ant.position).all(), 'position not updated'
+    assert np.isclose(1, np.linalg.norm(ant.momentum)).all(), 'momentum not one: %r' % ant.momentum
+    assert np.isclose(1, np.linalg.norm(ant.position - previous_position)).all(), 'movement not one: %r' % ant.position
