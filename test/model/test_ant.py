519fb9cbf50baa790dc2dca8d3ced0c4623092b6
ssn2500 <saeed.salehi.n@gmail.com>
2019-05-21 16:00:24 +0200

    ant move updated and test script created (but empty)

--- test/model/test_ant.py
+++ test/model/test_ant.py
@@ -1,8 +1,33 @@
+from src.model.ant import Ant
+from src.model.nest import Nest
+from src.utils import array
+import numpy as np
 
-def test_move_has_food():
-    return
+nest = Nest(position=array([0, 0]), color='red', size=10, health=100)
+ant = Ant(color='red', home_nest=nest)
 
 
-def test_move_randomly():
-    return
+def test_update_has_food():
+    ant.has_food = True
+    ant.position = array([10, 0])
+    position = ant.move()
+    # asserting that y-move is towards the nest
+    assert np.isclose(position, array([9, 0])) == array([True, True]), 'incorrect y-move direction'
+    # asserting that the position is updated
+    assert np.isclose(ant.position, position) == array([True, True]), 'position not updated'
+    ant.position = array([0, 10])
+    position = ant.move()
+    # asserting that x-move is towards the nest
+    assert np.isclose(position, array([0, 9])) == array([True, True]), 'incorrect x-move direction'
 
+
+def test_update__randomly():
+    ant.has_food = False
+    ant.position = array([0, 0])
+    ant.momentum = array([0, 0])
+    for i in range(10):  # trying 10 random walks
+        previous_position = array(ant.position)
+        position = ant.move()
+        assert np.isclose(position, ant.position) == array([True, True]), 'position not updated'
+        assert not np.isclose(1, np.linalg.norm(ant.momentum)), 'momentum not one: %r' % ant.momentum
+        assert not np.isclose(1, np.linalg.norm(ant.position-previous_position)), 'movement not one: %r' % ant.position
