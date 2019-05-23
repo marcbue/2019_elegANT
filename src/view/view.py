968b6fffae5c5ba4c3b72299f18ef7b8deb16236
Lukas Braun <mail@lukasbraun.com>
2019-05-23 23:16:59 +0200

    Add world object which contains all game elements

--- src/view/view.py
+++ src/view/view.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 from .text import Text
 from .button import Button
@@ -8,9 +9,8 @@ from .ant import Ant
 from .world import World
 from src.utils import array
 
-# import numpy as np
-import platform
 
+# import numpy as np
 
 # View
 class View:
@@ -19,30 +19,13 @@ class View:
 
     def __init__(self, width, height):
         pygame.init()
-        display_info = pygame.display.Info()
 
-        # Currently not used
-        #width = display_info.current_w
-        #height = display_info.current_h
-        self.width = width
-        self.height = height
         self.state = None
-
-        # Only works for windows --> need to check operating system
-        
-        if platform.system() == 'Windows':
-            from ctypes import windll
-            windll.user32.SetProcessDPIAware()
-            true_res = (windll.user32.GetSystemMetrics(0), windll.user32.GetSystemMetrics(1))
-            self.screen = pygame.display.set_mode(true_res, pygame.FULLSCREEN)
-
-        #else:
-        #    print(platform.system())
-        #    self.screen = pygame.display.set_mode((self.width, self.height), pygame.FULLSCREEN)
-        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.size = width, height
+        self.screen = pygame.display.set_mode(self.size)
         self.background_color = pygame.Color("white")
-        self.mouse_pos = None
-        
+        self.mouse_pos = pygame.mouse.get_pos()
+        self.mouse_event = pygame.mouse.get_pressed()
         self.elements = {}
         self.event_dict = {}
         self.FONT = pygame.font.Font(None, 32)
@@ -64,9 +47,9 @@ class View:
 
     def _start_view(self):
         self.elements = {}
+
         # add elements for the main text
-        text = Text(self, "headline", 0 + int(0.15 * self.width), 0 + int(0.10 * self.height), -1, -1,
-                    int(0.06 * self.width))
+        text = Text(self, "headline", 250, 100, -1, -1, 115)
         text.set_text("ElegANT")
         self.add_element(text)
 
@@ -88,26 +71,14 @@ class View:
 
         # Add start game event
         start_button.on("click", lambda: self.event_dict.update({"start_button":
-                                                                     (self.get_element_by_id(
-                                                                         "color_selector").get_selection(),
-                                                                      self.get_element_by_id("textbox").text)}))
+                        (self.get_element_by_id("color_selector").get_selection(),
+                            self.get_element_by_id("textbox").text)}))
 
         self.add_element(start_button)
 
-        starttext = Text(self, "starttext", 0 + int(0.125 * self.width), 0 + int(0.835 * self.height), -1, -1,
-                         int(0.025 * self.width))
+        starttext = Text(self, "starttext", 225, 650, -1, -1, 50)
         starttext.set_text("START")
         self.add_element(starttext)
-
-        quit_button = Button(self, "quit_button", 0 + int(0.9825 * self.width), 0 + int(0.0080 * self.height),
-                             int(0.015 * self.width), int(0.015 * self.width),
-                             -1, (250, 0, 0), (150, 150, 150), 'square')
-        self.add_element(quit_button)
-
-        quittext = Text(self, "quittext", 0 + int(0.99 * self.width), 0 + int(0.02 * self.height), -1, -1,
-                        int(0.010 * self.width))
-        quittext.set_text("X")
-        self.add_element(quittext)
 
         inputname = Text(self, "inputname", 220, 250, -1, -1, 30)
         inputname.set_text("Please enter your name")
@@ -118,24 +89,9 @@ class View:
 
     def _game_view(self):
         self.elements = {}
-        
-        # add move map
+
+        # Create world which contains all game objects
         self.add_element(World(self, "world", 0, 0, 250, 250))
-
-        # add a nest and an ant
-        #self.add_element(Nest(self, "nest", 650, 400, 30, (220, 0, 0)))  # red
-        #self.add_element(Ant(self, "ant", 660, 500, 10, (220, 0, 0)))  # peach
-
-        # add quit button
-        quit_button = Button(self, "quit_button", 0 + int(0.9825 * self.width), 0 + int(0.0080 * self.height),
-                             int(0.015 * self.width), int(0.015 * self.width),
-                             -1, (250, 0, 0), (150, 150, 150), 'square')
-        self.add_element(quit_button)
-
-        quittext = Text(self, "quittext", 0 + int(0.99 * self.width), 0 + int(0.02 * self.height), -1, -1,
-                        int(0.010 * self.width))
-        quittext.set_text("X")
-        self.add_element(quittext)
 
         # TODO add sliders to the game view
         # self.add_element(
@@ -168,16 +124,23 @@ class View:
         pygame.display.flip()
 
     def update(self, game_state):
-        #if game_state:
-        #    self.elements["world"].update(game_state)
-        self.elements["world"].update(game_state)
+        if game_state:
+            self.elements["world"].update(game_state)
 
     def events(self):
         self.mouse_pos = pygame.mouse.get_pos()
         self.event_dict = {}
 
         for event in pygame.event.get():
-            for element in self.elements.values():
-                element.event_handler(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            else:
+                for element in self.elements.values():
+                    element.event_handler(event)
+
+        if self.event_dict:
+            print(self.event_dict)
 
         return self.event_dict
+
