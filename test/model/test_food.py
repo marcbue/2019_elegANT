c1c163ba1a8bfd0fc9872d385453f6b88e06ce15
Zainab-mohamed <Zainabrajab14@gmail.com>
2019-05-25 16:22:38 +0200

    tests added

--- test/model/test_food.py
+++ test/model/test_food.py
@@ -1,11 +1,11 @@
 from src.model.food import Food
 import pytest
-import array
+from src.utils import array
 
 
 @pytest.fixture
 def set_up_init_food_fixed():
-    position = array.array('f', [0.5, 0.5])
+    position = array([0.5, 0.5])
     size1 = 5
     size2 = -2
     return position, size1, size2
@@ -14,14 +14,14 @@ def set_up_init_food_fixed():
 def test__init__(set_up_init_food_fixed):
     position, size1, size2 = set_up_init_food_fixed
     food = Food(position, size1)
-    assert food.position[:] == position[:] and food.size == size1
+    assert ((food.position == position).all()) and food.size == size1
 
 
 def test_update_food(set_up_init_food_fixed):
     position, size1, size2 = set_up_init_food_fixed
     food1 = Food(position, size1)
     new_position1 = food1.update(size1)
-    assert new_position1[:] == position[:]
+    assert ((new_position1 == position).all())
     food2 = Food(position, size2)
     new_position2 = food2.update(size2)
     assert new_position2 is None
