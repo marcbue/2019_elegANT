319863bbb956a7afc1db1dfe3fb8b1d0c4d12680
Lukas Braun <mail@lukasbraun.com>
2019-06-04 16:53:30 +0200

    Add image buttons to color selector and make it circular

--- src/view/button.py
+++ src/view/button.py
@@ -4,14 +4,13 @@ from .ui_element import UIElement
 
 class Button(UIElement):
     def __init__(self, view, identifier, x, y, width, height, radius, color1, color2,
-                 shape='circle', has_image='False', image_path=''):
+                 shape='circle', has_image=False, image_path=''):
         super(Button, self).__init__(view, identifier, x, y, width, height)
         self.color1 = color1
         self.color2 = color2
         self.color = color1
         self.radius = radius
         self.shape = shape
-        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
         self.has_image = has_image
         self.image_path = image_path
 
@@ -21,8 +20,11 @@ class Button(UIElement):
     def draw(self):
         if self.shape == 'circle':
             pygame.draw.circle(self.view.screen, self.color, (self.x, self.y), self.radius)
+            if self.has_image is True:
+                image = pygame.transform.scale(pygame.image.load(self.image_path), (self.width, self.height))
+                self.view.screen.blit(image, pygame.Rect(self.x - (self.width // 2), self.y - (self.height // 2), self.width, self.height))
         elif self.shape == 'square':
             pygame.draw.rect(self.view.screen, self.color, (self.x, self.y, self.width, self.height))
-        if self.has_image is True:
-            image = pygame.transform.scale((pygame.image.load(self.image_path)), (self.width, self.height))
-            self.view.screen.blit(image, self.rect)
+            if self.has_image is True:
+                image = pygame.transform.scale(pygame.image.load(self.image_path), (self.width, self.height))
+                self.view.screen.blit(image, pygame.Rect(self.x, self.y, self.width, self.height))
