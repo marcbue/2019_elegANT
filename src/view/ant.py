48f98a80fc2ae403e7a7efd65b1b859ee3e75ff8
Lukas Braun <mail@lukasbraun.com>
2019-06-05 17:14:40 +0200

    Add images and rotation to ants

--- src/view/ant.py
+++ src/view/ant.py
@@ -1,13 +1,32 @@
 import pygame
+import math
 from .view_element import ViewElement
 
 
 class Ant(ViewElement):
-    def __init__(self, view, identifier, x, y, radius, color, shape='circle'):
-        super(Ant, self).__init__(view, identifier, x, y, width=radius * 2, height=radius * 2)
+    def __init__(self, view, identifier, x, y, color):
+        super(Ant, self).__init__(view, identifier, x, y, width=64, height=64)
         self.color = color
-        self.radius = radius
-        self.shape = shape
+        self.direction = [0., 0.]
+        self.has_food = False
+        name = "_".join((str(c) for c in color))
+        self.img = pygame.image.load("src/view/images/219_95_87_scout.png")
+        
+        self.rotation = 0
 
     def draw(self):
-        pygame.draw.circle(self.view.screen, self.color, (self.x, self.y), self.radius)
+        rotation = math.atan2(self.direction[0], self.direction[1]) * (180 / math.pi) + 180
+        self.rotation += 5
+        
+        loc = self.img.get_rect().center
+        rot_sprite = pygame.transform.rotate(image, angle)
+        rot_sprite.get_rect().center = loc
+    
+        pivot = pygame.math.Vector2(self.width/2, -self.height/2)
+        pivot_rotate = pivot.rotate(self.rotation)
+        pivot_move = pivot_rotate - pivot
+        
+        origin = (self.x - pivot_move[0], self.y + pivot_move[1])
+        
+        img = pygame.transform.rotate(self.img, self.rotation)
+        self.view.screen.blit(img, origin)
--- src/view/world.py
+++ src/view/world.py
@@ -5,11 +5,9 @@ from .view_element import ViewElement
 from .nest import Nest
 from .ant import Ant
 
-# TODO check if these imports are necessary
-# from src.model.game_object import GameObject
-# from src.model.kd_tree_and_dict import KdTreeAndDict
 from src.model.nest import Nest as Model_Nest
 from src.model.ant import Ant as Model_Ant
+from src.model.ant import Food as Model_Food
 
 
 class World(ViewElement):
@@ -64,6 +62,9 @@ class World(ViewElement):
                 # Update position
                 view_element = self.game_elements[element.id]
                 view_element.x, view_element.y = self._to_view_coordinates(element.position)
+                if type(element) == Model_Ant:
+                    view_element.direction = element.direction
+                    print(element.direction)
             else:
                 view_x, view_y = self._to_view_coordinates(element.position)
                 color = (255, 0, 0)  # element.color
@@ -73,7 +74,9 @@ class World(ViewElement):
                     self.game_elements[element.id] = Nest(self.view, element.id, view_x, view_y, 30, color)
 
                 elif type(element) == Model_Ant:
-                    self.game_elements[element.id] = Ant(self.view, "ant", view_x, view_y, 10, (220, 0, 0))
+                    self.game_elements[element.id] = Ant(self.view, "ant", view_x, view_y, (220, 0, 0))
+                elif type(element) == Model_Food:
+                    pass
                 else:
                     print(f"Should create {element}")
 
